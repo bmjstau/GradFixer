3c0135b5f3242990a0b63200b52c36613a01ed5c
bmjstau <bmjstau@users.noreply.github.com>
2019-03-13 14:47:31 +0100

    Switched over to fieldtrip utility functions.

--- fix_CTF_grad.m
+++ fix_CTF_grad.m
@@ -5,10 +5,6 @@ function [data] = fix_CTF_grad(headerpath, data)
 % Headposition must have been recorded continously in the CTF headtracking 
 % channels. Has (so far) only been tested for CTF275 systems.
 %
-% For clarity: The chanori- and coiloir-fields have unit vector coordinates
-% from chanpos/coilpos (shifted to the origin) towards the orientation of 
-% the channel/coil.
-%
 % Use as
 %   data = ft_fix_grad(headerpath, data)
 %
@@ -68,66 +64,15 @@ mN = mean(coil1, 2).*100; % Nasion
 mL = mean(coil2, 2).*100; % LPA
 mR = mean(coil3, 2).*100; % RPA
 
-%% Find unit vectors of the new coordinate system, project old coordinates
-% onto them.
+% Get transform matrix, apply it to chan- and coilpos and -ori
+[h, coordsys] = ft_headcoordinates(mN, mL, mR, [], 'ctf');
+newGrad = ft_transform_geometry(h, dewar_coord);
+newGrad.coordsys = coordsys;
 
-% The X-Axis is the unit normal of the vector from the origin (midpoint 
-% between LPA and RPA) to the nasion.
-origin = mean([mL mR],2);
-xNorm = (mN - origin) / norm(mN - origin);
-
-% The Z-Axis is in the direction of the unit normal of the plane spanned by
-% the three fiducial coils - for an explanation, see
-% http://mathworld.wolfram.com/Point-PlaneDistance.html
-% Compute the unit normal from the XY-Plane
-zNorm = (cross((mL - mN), (mR - mN))) / ...
-    norm(cross((mL - mN), (mR - mN)));
-
-% The Y-Axis is perpendicular to both X- and Z_axis, so we can just use
-% their crossproduct. The norm isn't really necessary, but let's keep it
-% for consistency. The Y-Axis in CTF is defined as going to the left 
-% - so we flip it from the cross product, which goes to the right.
-yNorm = - (cross(xNorm, zNorm) / norm(cross(xNorm, zNorm)));
-
-% Apply new coordinate system to channel positions and orientations.
-for iChan = 1:length(dewar_coord.chanpos)
-    iChanpos = dewar_coord.chanpos(iChan,:)';
-    iChanori = dewar_coord.chanori(iChan,:)';
-    
-    chanPos(iChan,1) = dot(xNorm, (iChanpos - origin));
-    chanPos(iChan,2) = dot(yNorm, (iChanpos - origin));
-    chanPos(iChan,3) = dot(zNorm, (iChanpos - origin));
-        
-    chanOri(iChan,1) = dot(xNorm, iChanori);
-    chanOri(iChan,2) = dot(yNorm, iChanori);
-    chanOri(iChan,3) = dot(zNorm, iChanori);
-end
-
-% Apply new coordinate system to coil positions and orientations.
-for iCoil = 1:length(dewar_coord.coilpos)
-    iCoilpos = dewar_coord.coilpos(iCoil,:)';
-    iCoilori = dewar_coord.coilori(iCoil,:)';
-    
-    coilPos(iCoil,1) = dot(xNorm, (iCoilpos - origin));
-    coilPos(iCoil,2) = dot(yNorm, (iCoilpos - origin));
-    coilPos(iCoil,3) = dot(zNorm, (iCoilpos - origin));
-        
-    coilOri(iCoil,1) = dot(xNorm, iCoilori);
-    coilOri(iCoil,2) = dot(yNorm, iCoilori);
-    coilOri(iCoil,3) = dot(zNorm, iCoilori);
-end
-    
 %% Replace grad field of the data structure with the headmovement-corrected
 % grad.
-ft_notice(['replacing channel- and coil-orientation and -position with '...
-    'headmotion-corrected ones']);
-newGrad = data.grad;
-newGrad.chanpos = chanPos;
-newGrad.chanori = chanOri;
-newGrad.coilpos = coilPos;
-newGrad.coilori = coilOri;
-
+ft_notice(['Replacing channel- and coil-orientation and -position with '...
+    'headmotion-corrected ones.']);
 data.grad = newGrad;
 
 end
-
